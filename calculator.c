abf810b54bcadf409780fc27aea3925d08d8ee24
hilshprajapati501-hue <hilshprajapati501@gmail.com>
2026-05-12 17:03:26 +0530

    Create calculator.c
    basic operator

---
+++ calculator.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a = 15, b = 5;
+    scanf("%d %d",&a,&b);
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Multi: %d\n", a * b);
+    printf("Div: %d\n", a / b);
+    printf("Pct: %d\n", a % b);
+    return 0;
+}
