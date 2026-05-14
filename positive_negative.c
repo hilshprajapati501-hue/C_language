a9a1054277957dea1f89fa410ce20dec482ee00c
hilshprajapati501-hue <hilshprajapati501@gmail.com>
2026-05-14 17:31:28 +0530

    file_created

---
+++ positive_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int numm;
+    scanf ("%d",numm);
+    
+    if (numm>0)
+        {
+            printf("Positive");
+        }
+        else
+        {
+            printf("Negative");
+        }
+    return 0;
+}
