2712054feac467a59a4b8b1e87881acaed89b274
hilshprajapati501-hue <hilshprajapati501@gmail.com>
2026-05-15 16:57:50 +0530

    Create ternary_operator.c

---
+++ ternary_operator.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    
+    printf("Enter the value of age :-");
+    scanf("%d",&age);
+    
+    age>=18?printf("elegible for voting"):printf("not elegible");
+    return 0;
+}
