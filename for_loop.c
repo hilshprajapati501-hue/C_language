deac2612f9042237b40a12fe6dad015c7e3d5214
hilshprajapati501-hue <hilshprajapati501@gmail.com>
2026-06-02 16:56:53 +0530

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 0;i<=10;i++)
+    {
+        int a = 11;
+        
+        printf("This is the for loop and its iteration number is %d\n",a-i);
+    }
+
+
+    return 0;
+}
